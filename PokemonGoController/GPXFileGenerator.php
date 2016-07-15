d20e30ffd16ae7d6196035ad7f35da8685e3350b
anthann <72115165g@gmail.com>
2016-07-15 15:27:43 +0800

    fix the bug that GPXFileGenerator.php set wrong values to keys

--- PokemonGoController/GPXFileGenerator.php
+++ PokemonGoController/GPXFileGenerator.php
@@ -2,7 +2,7 @@
 	$latitude = $_POST["latitude"];
 	$longitude = $_POST["longitude"];
 
-	$gpx = '<gpx creator="Xcode" version="1.1"><wpt lat="'.$longitude.'" lon="'.$latitude.'"><name>PokemonLocation</name></wpt></gpx>';
+	$gpx = '<gpx creator="Xcode" version="1.1"><wpt lat="'.$latitude.'" lon="'.$longitude.'"><name>PokemonLocation</name></wpt></gpx>';
 	
 	// if you don't have permission to write file, execute the command below
 	// chmod 777 /Users/zwb/Developer/iOS/PokemonHandler/PokemonHandler/pokemonLocation.gpx
